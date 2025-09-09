00175536f6ba85d2a1e52a1d76eca68d0411d085
onarpita <onarpita14@gmail.com>
2025-09-09 20:41:23 +0600

    show plant categories

--- script.js
+++ script.js
@@ -29,3 +29,105 @@ const showAllCategories = (categories) => {
         `;
     });
 }
+const loadAllPlants = async() => {
+    manageLoader(true);
+    const url = "https://openapi.programming-hero.com/api/plants";
+    const res = await fetch(url);
+    const data = await res.json();
+    showAllPlants(data.plants);
+}
+
+const showAllPlants = (plants) => {
+    removeActive();
+    const allTreeBtn = document.getElementById("tree-all");
+    allTreeBtn.classList.add("bg-[#15803d]", "text-white");
+    plantCards.innerHTML = "";
+    plants.forEach(plant => {
+        plantCards.innerHTML += `
+            <div class="card bg-base-100 w-full shadow-sm">
+                <figure class="px-4 pt-4">
+                    <img src="${plant.image}" alt="${plant.name}"
+                        class="rounded-xl w-full h-50 object-cover" />
+                </figure>
+                <div class="card-body pb-4">
+                    <h2 class="card-title cursor-pointer" onclick="loadPlantDetail('${plant.id}')">${plant.name}</h2>
+                    <p>${plant.description}</p>
+                    <div class="flex justify-between items-center">
+                        <div class="rounded-full bg-[#dcfce7] text-[#15803d] py-1 px-3 font-semibold">${plant.category}</div>
+                        <div class="font-semibold">৳${plant.price}</div>
+                    </div>
+                </div>
+                <div class="pb-4 px-4">
+                    <button class="btn bg-[#15803d] btn-block rounded-full text-white">Add to Cart</button>
+                </div>
+            </div>
+        `;
+    });
+    manageLoader(false);
+}
+
+const removeActive = () => {
+    const allCategories = document.querySelectorAll("#category-list li");
+    allCategories.forEach(category => {
+        category.classList.remove("bg-[#15803d]", "text-white");
+    });
+}
+
+const loadPlantsByCategory = async(id) => {
+    manageLoader(true);
+    const url = `https://openapi.programming-hero.com/api/category/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    showPlantsByCategory(data.plants, id);
+}
+
+const showPlantsByCategory = (plants, id) => {
+    removeActive();
+    const clickedCategory = document.getElementById(`tree-${id}`);
+    clickedCategory.classList.add("bg-[#15803d]", "text-white");
+    plantCards.innerHTML = "";
+    plants.forEach(plant => {
+        plantCards.innerHTML += `
+            <div class="card bg-base-100 w-full shadow-sm">
+                <figure class="px-4 pt-4">
+                    <img src="${plant.image}" alt="${plant.name}"
+                        class="rounded-xl w-full h-50 object-cover" />
+                </figure>
+                <div class="card-body pb-4">
+                    <h2 class="card-title cursor-pointer" onclick="loadPlantDetail('${plant.id}')">${plant.name}</h2>
+                    <p>${plant.description}</p>
+                    <div class="flex justify-between items-center">
+                        <div class="rounded-full bg-[#dcfce7] text-[#15803d] py-1 px-3 font-semibold">${plant.category}</div>
+                        <div class="font-semibold">৳${plant.price}</div>
+                    </div>
+                </div>
+                <div class="pb-4 px-4">
+                    <button class="btn bg-[#15803d] btn-block rounded-full text-white">Add to Cart</button>
+                </div>
+            </div>
+        `;
+    });
+    manageLoader(false);
+}
+
+const loadPlantDetail = async(id) => {
+    const url = `https://openapi.programming-hero.com/api/plant/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    showPlantDetail(data.plants);
+}
+
+const showPlantDetail = (plant) => {
+    const plantModal = document.getElementById("plant_modal");
+    const plantDetail = document.getElementById("plant-detail");
+    plantModal.showModal();
+    plantDetail.innerHTML = `
+        <h3 class="text-2xl font-semibold">${plant.name}</h3>
+        <img src="${plant.image}" alt="${plant.name}" class="w-full h-60 object-cover rounded-xl">
+        <p><strong>Category:</strong> ${plant.category}</p>
+        <p><strong>Price:</strong> ৳${plant.price}</p>
+        <p><strong>Description:</strong> ${plant.description}</p>
+    `;
+}
+
+plantCards.addEventListener('click', (e) => addToCart(e))
